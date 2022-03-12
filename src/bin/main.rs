0e4bb87ac00b97e7f5a8b4da2829441890794f5a
Peter Stuart <peter@peterstuart.org>
2022-03-12 17:22:57 -0500

    Use max clock speed

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -4,7 +4,7 @@
 use piano_viz as _; // global logger + panicking-behavior + memory layout
 use piano_viz::LEDs;
 use smart_leds::hsv::Hsv;
-use stm32f4xx_hal::{pac, prelude::*, time::Hertz};
+use stm32f4xx_hal::{pac, prelude::*, rcc::SYSCLK_MAX, time::Hertz};
 use ws2812_timer_delay::Ws2812;
 
 const NUM_LEDS: usize = 144;
@@ -23,7 +23,7 @@ fn main() -> ! {
     ) {
         // Constrain clocking registers
         let rcc = dp.RCC.constrain();
-        let clocks = rcc.cfgr.sysclk(Hertz::MHz(48)).freeze();
+        let clocks = rcc.cfgr.sysclk(Hertz::Hz(SYSCLK_MAX)).freeze();
 
         let mut timer = cp.SYST.counter_hz(&clocks);
         timer.start(Hertz::MHz(3)).unwrap();
